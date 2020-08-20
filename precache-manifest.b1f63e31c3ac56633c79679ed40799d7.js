88e80fa5337cff33aa9db793fdd1e320a1e120cb
vertalet76crew <vertalet76crew@gmail.com>
2020-08-20 20:43:37 +0400

    Updates

---
+++ precache-manifest.b1f63e31c3ac56633c79679ed40799d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2148952a9233be885d5a1a49aa886bf",
+    "url": "/cat-food/index.html"
+  },
+  {
+    "revision": "b97aefcd888297c19327",
+    "url": "/cat-food/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "7a2f2c710fb821ce3da8",
+    "url": "/cat-food/static/css/main.5a2856c1.chunk.css"
+  },
+  {
+    "revision": "b97aefcd888297c19327",
+    "url": "/cat-food/static/js/2.8759a0d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cat-food/static/js/2.8759a0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a2f2c710fb821ce3da8",
+    "url": "/cat-food/static/js/main.d5879475.chunk.js"
+  },
+  {
+    "revision": "04d12276cb23ac32ceb1",
+    "url": "/cat-food/static/js/runtime-main.c3a21e33.js"
+  },
+  {
+    "revision": "0eb38956fc16817daeb2a283e2328b06",
+    "url": "/cat-food/static/media/backgground.0eb38956.jpg"
+  },
+  {
+    "revision": "4766b0a286dd6f1c2d6ea007edb45ed0",
+    "url": "/cat-food/static/media/cat.4766b0a2.png"
+  }
+]);
